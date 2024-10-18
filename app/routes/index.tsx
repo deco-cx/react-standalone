5b8423ffa4da1b04f332330841974490fabccb10
gimenes <tlgimenes@gmail.com>
2024-10-18 10:47:42 -0300

    add landing page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,32 @@
 import { useLoaderData } from "react-router-dom";
 
 export const loader = () => {
-  return { target: "React Router!" };
+  return {
+    features: [
+      "React",
+      "TypeScript",
+      "TailwindCSS",
+      "React Router",
+      "HMR powered",
+    ],
+  };
 };
 
 export default function Page() {
   const data = useLoaderData();
 
   return (
-    <div>
-      Hello {data.target}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800 w-full">
+      <h1 className="text-4xl font-bold mb-4">Welcome to React Standalone!</h1>
+      <p className="text-lg mb-2">
+        Experience the power of React running directly in your browser.
+      </p>
+      <p className="text-lg mb-2">
+        No server required, just pure client-side magic!
+      </p>
+      <p className="text-lg mt-4 font-bold">
+        {data.features.join(", ")}!
+      </p>
     </div>
   );
 }
