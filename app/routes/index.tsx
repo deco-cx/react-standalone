8615e053042ebdfdecf63ab776ba0e5019eeea2f
gimenes <tlgimenes@gmail.com>
2024-10-18 10:49:06 -0300

    more styles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -17,7 +17,7 @@ export default function Page() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800 w-full">
-      <h1 className="text-4xl font-bold mb-4">Welcome to React Standalone!</h1>
+      <h1 className="text-4xl font-bold mb-4 text-blue-600">Welcome to React Standalone!</h1>
       <p className="text-lg mb-2">
         Experience the power of React running directly in your browser.
       </p>
