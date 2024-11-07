b28428a4e0e58aa5c14ce58025d28899924dbb78
igorbrasileiro <brasileiro456@gmail.com>
2024-11-07 16:57:47 -0300

    Upgrade cache key

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { getSDK } from "https://webdraw.ai/webdraw-sdk";
 
-const cache = await caches.open("react-standalone::v3");
+const cache = await caches.open("react-standalone::v4");
 
 function filepathToMimeType(filepath) {
   if (/\.(tsx?|jsx?)$/.test(filepath)) {
